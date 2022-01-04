af4b072c66e47cff889f2dd15351a6e5f4560e41
Sohail Gsais <Gsais.sohail9@gmail.com>
2022-01-04 14:44:48 +0000

    added server paths

--- server.js
+++ server.js
@@ -1,30 +1,21 @@
 var express = require("express");
 var app = express();
-var path = require("path");
-var http = require("http");
+app.use(express.static("public"));
 
-
-
-
- app.use(express.static("public"));
-    
-
-// Routes GET requests to /olivia to the request handler
+// Routes GET requests to /lessons to the request handler
 app.get("/lessons", function (request, response) {
-  response.send(
-   [ { topic: "math", location: "London", price: 100 },
+  response.send([
+    { topic: "math", location: "London", price: 100 },
     { topic: "math", location: "Liverpool", price: 80 },
     { topic: "math", location: "Oxford", price: 90 },
-    { topic: "math", location: "Bristol", price: 120 }]
-  );
+    { topic: "math", location: "Bristol", price: 120 },
+  ]);
 });
+
+// GET requests to /users
 app.get("/user", function (request, response) {
-    response.send(
-        {'email': 'user@email.com', 'password':'mypassword'}
-
-    );
-  });
-
+  response.send({ email: "user@email.com", password: "mypassword" });
+});
 
 // if you load something other that /olivia, servers a 404 error.
 // if itsnot a GET request also servers a 404 error.
@@ -32,5 +23,5 @@ app.use(function (request, response) {
   response.status(404).send("page not found");
 });
 
-
 app.listen(3000);
+console.log("Listening on port " + 3000);
